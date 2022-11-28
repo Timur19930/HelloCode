ff7310637ca7732e4056c6848362d957c96fa289
Timur <golubev_t@mail.ru>
2022-11-28 23:26:44 +0300

    commit7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите имя");
+﻿Console.Write("Введите ваше имя");
+Console.Write("Введите имя");
 string username = Console.ReadLine();
 Console.Write("Привет");
-Console.WriteLine(username);
+Console.Write(username);
